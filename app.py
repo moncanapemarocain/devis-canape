15435c8cd15ed53ce7d0b31203d883324b404200
moncanapemarocain <commercial@moncanapemarocain.com>
2025-11-26 13:56:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -509,7 +509,7 @@ with tab4:
     # Les éléments liés aux coussins étaient auparavant dans les options ; ils sont déplacés ici.
     type_coussins = st.selectbox(
         "Type de coussins",
-        ["auto", "65", "80", "90", "80-90", "valise", "p", "g"],
+        ["auto", "65", "80", "90", "80-90", "valise", "g"],
         help="Auto = optimisation automatique, 80-90 = optimisation par côté entre 80 et 90 cm"
     )
 
